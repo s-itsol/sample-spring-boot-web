27b1f3518a07a8c97c46f94619a19a5251c0e4b6
rei.shibano <shibano@s-itsol.net>
2022-06-10 13:57:23 +0900

    spring-initializerで、Warアプリを生成した直後の状態。

---
+++ src/test/java/net/sitsol/samplespringbootweb/SampleSpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sitsol.samplespringbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
